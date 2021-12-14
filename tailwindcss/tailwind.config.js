0064f173e066cf4554ef47c6662e3cd4d36c61f2
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2021-12-14 22:44:28 +0100

    Second commit

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
     purge: ["../*.html"],
     content: [],
     theme: {
-        extend: {},
+        backgroundColor: theme => ({
+            ...theme('colors'),
+            'main-bg': 'hsl(270, 20%, 96%)',
+        })
+    },
+    fontFamily: {
+        'body': ["'Rubik', sans-serif;"],
+    },
+    textColor: {
+        'rightHeading': "hsl(271, 36%, 24%)",
     },
     plugins: [],
+}
+plugins: [],
 }
